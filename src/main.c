71eecab37a1cf8f1c3db649d7762d647e62d075a
octopus <2841776039@qq.com>
2023-06-24 20:03:44 +0800

    增加注释

--- src/main.c
+++ src/main.c
@@ -13,6 +13,13 @@
 
 #define BUFF_LEN 1024
 #define SERVER_PORT 53
+#define MAP_LEN 0x0fffffff
+
+
+// 获取一个udp请求,如果是来自客户端的请求,则进行解析,得到请求的网址
+// 如果网址在cache里面,就将结果返回
+// 如果网站不在,就向上级服务器请求,维护一个转发表
+// 如果udp来自上级服务器,就查表将结果返回给客户端
 
 int server_fd;
 struct sockaddr_in server_addr;
@@ -29,9 +36,9 @@ typedef struct {
     int recv_len;
 }thread_arg;
 
-// TODO: 改掉临时的缓存,加锁
-uint16_t buf_req_id[BUFF_LEN];
-thread_arg buf_arg[BUFF_LEN];
+// TODO: 改掉临时的转发表,加锁.
+uint16_t buf_req_id[MAP_LEN];
+struct sockaddr_in buf_sock[MAP_LEN];
 int buf1_len = 1;
 
 void *pthread_func(void *a){
@@ -51,12 +58,11 @@ void *pthread_func(void *a){
 
         // 获得id
         uint16_t req_id = header.id;
-        // 缓存req
+        // 缓存req,这里的缓存都是指的转发表
         // TODO: 别用这个弱智缓存
         buf_req_id[buf1_len] = req_id;
-        // TODO: 可以缓存指针,对应在解放后free,注意超时
-        buf_arg[buf1_len] = *arg;
-        if(buf1_len >= BUFF_LEN){
+        buf_sock[buf1_len] = arg->client_addr;
+        if(buf1_len >= MAP_LEN){
             buf1_len = 1;
         }
 
@@ -75,12 +81,12 @@ void *pthread_func(void *a){
         // 获得id
         uint16_t req_id = header.id;
 
-       // TODO: 检查ttl和有效性,这里默认缓冲有效
+       // TODO: 检查ttl和有效性,这里默认转发表有效
         uint8_t buf_relay[BUFF_LEN];
         header.id = buf_req_id[req_id];
         uint8_t * relay_data = dump_header(buf_relay, header);
         memcpy(relay_data, data, BUFF_LEN - sizeof(DnsHeader));
-        sendto(server_fd, buf_relay, arg->recv_len, 0, (struct sockaddr*)&(buf_arg[req_id].client_addr), sizeof(buf_arg[req_id].client_addr));
+        sendto(server_fd, buf_relay, arg->recv_len, 0, (struct sockaddr*)&(buf_sock[req_id]), sizeof(buf_sock[req_id]));
      
     }
 
@@ -123,8 +129,6 @@ int main(int argc, char const *argv[]){
         // printf("recvfrom client data: %s\n", buf);
         pthread_t tid;
         pthread_create(&tid, NULL, pthread_func, (void*)arg);
-        // pthread_join(tid, NULL);
-        // sendto(server_fd, buf, recv_len, 0, (struct sockaddr*)&client_addr, sock_len);
     }
 
     return 0;
